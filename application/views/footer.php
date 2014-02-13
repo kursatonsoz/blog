f410cd7cf1838fcd6505fff959264d6c1f32a2dc
Kürşat Önsöz <m.kursat11@gmail.com>
2014-02-13 20:45:48 +0200

    edited footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,13 +4,13 @@
     <div class="footer-container clearfix">
 
         <ul class="social-icons">
-            <li class="facebook"><a href="https://www.facebook.com/kursat.onsoz" target="_blank"><i class="fa fa-facebook"></i></a></li>
-            <li class="twitter"><a href="https://twitter.com/kursatonsoz" target="_blank"><i class="fa fa-twitter"></i></a></li>
-            <li class="github"><a href="https://github.com/kursatonsoz" target="_blank"><i class="fa fa-github"></i></a></li>
-            <li class="linkedin"><a href="http://www.linkedin.com/pub/k%C3%BCr%C5%9Fat-%C3%B6ns%C3%B6z/56/490/120" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-            <li class="googleplus"><a href="https://plus.google.com/u/0/+kursatonsoz" target="_blank"><i class="fa fa-google-plus"></i></a></li>
-            <li class="mail"><a href="mailto:m.kursat11@gmail.com"><i class="fa fa-envelope"></i></a></li>
-            <li class="rss"><a href="#rss"><i class="fa fa-rss"></i></a></li>
+            <li class="facebook"><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+            <li class="twitter"><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+            <li class="github"><a href="#" target="_blank"><i class="fa fa-github"></i></a></li>
+            <li class="linkedin"><a href="#" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+            <li class="googleplus"><a href="#" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+            <li class="mail"><a href="#"><i class="fa fa-envelope"></i></a></li>
+            <li class="rss"><a href="/feed"><i class="fa fa-rss"></i></a></li>
         </ul>
 
         <div class="copy-right">
